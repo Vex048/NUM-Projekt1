96a9a22f7bfdb1f5def7e097bb130bde35c4ed7f
Marcin <borowiak04@gmail.com>
2026-05-11 19:46:26 +0200

    [BENTO-AWS]: Fixes again v2

--- src/models/baseline_classifier.py
+++ src/models/baseline_classifier.py
@@ -1,13 +1,26 @@
 import torch.nn as nn
 from src.models.base_classifier import BaseClassifier
+import torch
+
 
 class BaselineClassifier(BaseClassifier):
     """
     Prosty CNN jako baseLine, potem można dać tu np. XgBoost
     """
-    def __init__(self, num_classes: int = 10, lr: float = 1e-3, class_names: list = None, class_weights: torch.Tensor = None):
-        super().__init__(num_classes=num_classes, lr=lr, class_names=class_names, class_weights=class_weights)
-        
+
+    def __init__(
+        self,
+        num_classes: int = 10,
+        lr: float = 1e-3,
+        class_names: list = None,
+        class_weights: torch.Tensor = None,
+    ):
+        super().__init__(
+            num_classes=num_classes,
+            lr=lr,
+            class_names=class_names,
+            class_weights=class_weights,
+        )
 
         self.model = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, padding=1),
@@ -19,5 +32,5 @@ class BaselineClassifier(BaseClassifier):
             nn.Flatten(),
             nn.Linear(32 * 8 * 8, 128),
             nn.ReLU(),
-            nn.Linear(128, num_classes)
+            nn.Linear(128, num_classes),
         )
